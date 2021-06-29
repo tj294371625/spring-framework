0d7ae09cfd83041f201e6f00e654b80de7faf79d
chinadaas <chinadaas@chinadaasdeMacBook-Pro-3.local>
2021-06-29 20:28:32 +0800

    添加调试模块

---
+++ spring-source-read/src/main/java/com/chinadaas/SourceReader.java
@@ -0,0 +1,15 @@
+package com.chinadaas;
+
+import com.chinadaas.entity.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SourceReader {
+
+	public static void main(String[] args) {
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+		Student stu = (Student) context.getBean("student");
+		System.out.println(stu);
+	}
+}
